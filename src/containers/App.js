74dc515d2976f2e887eab41a4fedee6c77f4ab7f
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-07-12 22:19:19 -0700

    CSS up Header

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,8 +7,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Curology Cucumbers</h1>
+          <div className="search-container">
+            <form>
+              <input
+                placeholder="Search movies by title..."
+                type="text"
+              />
+            </form>
+          </div>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
