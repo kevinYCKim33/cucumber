9467ac4a5110d9b2adfc0d7d783c1a04298d762b
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-07-12 22:47:44 -0700

    content margins set with lorem ipsum

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,6 +17,13 @@ class App extends Component {
             </form>
           </div>
         </header>
+
+        <div className="App-body">
+          <div className="filler">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </div>
+        </div>
+
         <footer>
           <a href="https://github.com/kevinYCKim33/cucumber" target="_blank" rel="noopener noreferrer" title="Github"><i className="fa fa-github-square"></i></a>
         </footer>
